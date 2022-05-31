91a403d4966d5a2f3c0c766c5fe909b07cb50090
TerrificTable55 <omegagaming189gmail.com>
2022-05-31 23:14:26 +0200

    Main File

--- src/main/java/xyz/terrific/Main.java
+++ src/main/java/xyz/terrific/Main.java
@@ -13,8 +13,8 @@ import javax.swing.*;
 public class Main {
 
     public static String resources = "src/main/resources/";
-    public static String json_url = "https://anonfiles.com/167bOdl7y9/test_json";
-    public static String jar_url = "https://anonfiles.com/N37eO1l1y0/test_jar";
+    public static String json_url = "https://anonfiles.com/167bOdl7y9/test_json"; // Change to your Client Download URl
+    public static String jar_url = "https://anonfiles.com/N37eO1l1y0/test_jar"; // Change to your Client Download URl
 
     public static InstallerFrame frame;
 
